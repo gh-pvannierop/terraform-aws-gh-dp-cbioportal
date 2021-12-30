b580a1ae4b7c3755e0edb758f247ac76c96ae940
Hamed Ahouran <hahouran@guardanthealth.com>
2021-12-30 16:21:11 -0500

    ec2 cbioportal instance added

---
+++ main.tf
@@ -0,0 +1,42 @@
+#Terraform Configuraiton
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Configure the GitHub Provider
+provider "github" {
+}
+
+terraform {
+  backend "s3" {
+  }
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${local.prefix_path}/private_subnet_ids"
+}
+
+data "aws_ssm_parameter" "publicsubnet_ids" {
+  name = "/${local.prefix_path}/public_subnet_ids"
+}
+
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${local.prefix_path}/vpc_id"
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+  aws_region                       = var.aws_region
+  account                      = var.account 
+  ami                          = var.ami
+  instance_type                = var.instance_type
+  name                         = var.name
+  vpc_id                       = data.aws_ssm_parameter.vpc_id.value
+  public_subnet_id             = element(split(",",data.aws_ssm_parameter.public_subnet_ids.value),0)
+  #private_subnet_id            = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value),0)
+  #private_ip                   = var.private_ip
+  root_vol_size                = var.root_vol_size
+  root_vol_type                = var.root_vol_type
+  whitelist_cidr               = var.whitelist_cidr
+}
